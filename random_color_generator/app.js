c73f206f9a627d28ec3a232736f110798754d9f0
Simon <stribino@ea.com>
2021-09-07 17:11:21 +0200

    first bunch of projects

---
+++ random_color_generator/app.js
@@ -0,0 +1,24 @@
+const randomNumber = (min, max) => 
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomRGB = () => 
+[
+    randomNumber(0, 255),
+    randomNumber(0, 255),
+    randomNumber(0, 255)
+];
+
+const currenValueSpan = document.getElementById("value");
+
+document.getElementById("generate").addEventListener("click", function(e)
+{
+    const rgb = randomRGB();
+    const content = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    
+    document.documentElement.style.setProperty('--current-value', content);
+    currenValueSpan.textContent = content;
+})
